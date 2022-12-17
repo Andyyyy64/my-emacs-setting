7d0bc850be60cc033a2a532c69ffbd7f59129d82
Andyyyy64 <takuyarossi77@gmail.com>
2022-12-17 20:49:18 +0900

    sogood

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 ;; ----------------
+
 ;; key bind
 ;; ----------------
 ;; C-h
@@ -10,14 +11,15 @@
 ;; window operation
 (global-set-key "\C-t" 'other-window)
 ;; vterm comand
-(global-set-key "\C-x\C-t" 'vterm)
+(global-set-key "\C-x\C-t" 'multi-vterm)
 
 ; ----
 ; preferences
 ; ----
 ; Enable automatic completion of parentheses
 (electric-pair-mode 1)
-
+;; diable scroolbar
+(scroll-bar-mode -1)
 ;; set default tab-width for 4
 (setq-default tab-width 4 indent-tabs-mode nil)
 ;; no startup msg
@@ -44,16 +46,51 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (defun my-startup-commands ()
   "Execute a series of commands at startup."
-  (vterm))
+  (beacon-mode)
+  (nyan-mode)
+  (multi-vterm))
 
 (my-startup-commands)
+
+;; recentf
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 25)
+(global-set-key "\C-x\C-r" 'recentf-open-files)
+
+;; slime
+(setq inferior-lisp-program "sbcl")
 
 ;; for automatic syntax highlight
 (global-font-lock-mode t)
 
+;; prettier configure
+(require 'prettier-js)
+(add-hook 'js-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+(add-hook 'typescript-mode-hook 'prettier-js-mode)
+(setq prettier-js-args '(
+                        "--trailing-comma" "all"
+                        "--single-quote" "true"
+                        "--print-width" "120"
+                        "--tab-width" "2"
+                        "--use-tabs" "false"
+                        "--semi" "true"
+                        "--bracket-spacing" "true"
+                        "--jsx-bracket-same-line" "false"
+                        "--arrow-parens" "avoid"
+                        "--prose-wrap" "always"
+                        "--end-of-line" "lf"
+                        ))
+
 
 ;; -------------use-package------------
 
+
+;; multivterm
+(use-package multi-vterm :ensure t)
+(use-package beacon-mode :ensure t)
 ;; for filetreeview
 (use-package neotree
   :ensure t
@@ -69,10 +106,10 @@
 
 
 ;; for ts
-(use-package typescript-mode
-  :ensure t
-  :mode "\\.ts\\'")
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+;;(use-package typescript-mode
+  ;;:ensure t
+  ;;:mode "\\.ts\\'")
+;;(add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
 
 ;; for react...
@@ -121,10 +158,8 @@
   :init (global-company-mode))
 
 
-
 ;; .emacs.d/init.el
 (server-start)
-
 
 
 (custom-set-variables
@@ -132,10 +167,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(dracula))
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("05626f77b0c8c197c7e4a31d9783c4ec6e351d9624aa28bc15e7f6d6a6ebd926" default))
- '(package-selected-packages '(web-mode dracula-theme vterm helm))
+   '("bddf21b7face8adffc42c32a8223c3cc83b5c1bbd4ce49a5743ce528ca4da2b6" "05626f77b0c8c197c7e4a31d9783c4ec6e351d9624aa28bc15e7f6d6a6ebd926" default))
+ '(package-selected-packages
+   '(nyan-mode prettier slime prettier-js beacon multi-vterm zoom gruber-darker-theme web-mode dracula-theme vterm helm))
  '(warning-suppress-log-types '((server))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
